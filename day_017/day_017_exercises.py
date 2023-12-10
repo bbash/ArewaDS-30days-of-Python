29eb021ae501913d523b65d95612ffee7bea3664
bbash <bsalisu2016@gmail.com>
2023-12-10 09:30:22 +0100

    Added Solution to exercise 17

---
+++ day_017/day_017_exercises.py
@@ -0,0 +1,15 @@
+"""_
+    EXERCISES: Day 17
+"""
+
+# Question 1: names = ['Finland', 'Sweden', 'Norway','Denmark',
+# 'Iceland', 'Estonia','Russia']. Unpack the first five countries
+# and store them in a variable nordic_countries, store Estonia and 
+# Russia in es, and ru respectively.
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+
+*nordic_countries, es, ru = names
+print(f"Nordic countries: {nordic_countries}")
+print(f'Estonia: {es}')
+print(f"Rusia: {ru}")
